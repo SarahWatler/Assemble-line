0eda4aada34bac94041ec78eabe33d67a5089a97
sarah.watler <sarah.watler@MU-C1-20.mullen.pvt.k12.co.us>
2017-01-31 11:52:53 -0700

    1

---
+++ AssembleLine.java
@@ -0,0 +1,41 @@
+
+public class AssembleLine
+	{
+
+		public static void main(String[] args)
+			{
+				public static double returnfeet ()
+					{
+						Sacnner userInput = new Scanner (System.in;
+						System.out.println("please enter your height");
+						return userInput.nextDouble();
+					}
+					
+					Public static double changetoinches(double x)
+					{
+						return x * 12
+					}
+					
+					public static double inchestocentimeters (double x)
+					{
+						return x / 2.54
+					}
+					
+					public static double centimeterstometers (double x)
+					{
+						return x * 0.01
+					}
+					
+					public static double meterstofeet (double x )
+					{
+						return x * 3.28084
+					}
+					
+					public static double feettoinches (double x)
+					{
+						return x / 12
+					}// TODO Auto-generated method stub
+
+			}
+		
+	}
